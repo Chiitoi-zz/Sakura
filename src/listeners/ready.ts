232289ced3215a69df7db02db39ff823f2ea63fb
Chiitoi <vrajakul@gmail.com>
2021-05-14 19:24:30 -0400

    feat: add ability to change bot presence

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,5 +1,5 @@
 import { Listener } from 'discord-akairo'
-import { Constants } from 'discord.js'
+import { Constants, PresenceStatusData } from 'discord.js'
 
 export default class ReadyListener extends Listener {
     constructor() {
@@ -11,7 +11,8 @@ export default class ReadyListener extends Listener {
 
     public async exec() {
         const totalGuilds = this.client.guilds.cache.size
-        await this.client.user.setActivity('@Sakura guide', { type: 'LISTENING' })
+        const status = (process.env.STATUS?.length && ['online', 'dnd', 'idle', 'invisible'].includes(process.env.STATUS) ? process.env.STATUS : 'online') as PresenceStatusData
+        await this.client.user.setPresence({ activity: { name: '@Sakura guide', type: 'LISTENING' }, status })
         console.log(`${ this.client.user.tag } is online!`)
         console.log(`${ this.client.user.username } is in ${ totalGuilds } guilds(s)!`)
     }
